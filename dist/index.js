cdba75b19483791db3e42e222de02a86db610b32
Nilton de Oliveira Diniz <SNTRIT39@firjan.com.br>
2023-03-02 21:41:31 -0300

    adicionando

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3002;
-app.get('/', (req, res) => {
-    res.send('Meu primeiro server com Express e Typescript');
+app.use(express_1.default.json());
+app.get('/users', (req, res) => {
+    res.send('GET - Usado para obter dados');
+});
+app.get('/sum/:num1/:num2', (req, res) => {
+    const sum = Number(req.params.num1) + Number(req.params.num2);
+    res.send(`O resultado da soma é: ${sum}`);
+});
+app.get('/users/:id', (req, res) => {
+    //FINGE QUE EU FUI NO BANCO E TROUXE ESSAS INFOS
+    const user = {
+        "user": {
+            "nome": "Nilton",
+            "telefone": "249888888",
+            "endereco": "Rua sei que lá, 0, Tres Rios, RJ"
+        }
+    };
+    const response = {
+        "status-code": 200,
+        "message": "Usuário encontrado",
+        "data": user
+    };
+    res.send(response);
+});
+app.post('/users', (req, res) => {
+    //Parte do codigo responsavel por chamar um controller
+    //que chamar um model, que vai persistir a informação no banco.
+    var user = req.body.user;
+    user.id = 1;
+    const response = {
+        "status-code": 201,
+        "message": "Usuário criado com sucesso",
+        "user-registered": user
+    };
+    res.send(response);
+});
+app.put('/users/:id', (req, res) => {
+    res.send('PUT - Usado para atualizar dados');
+});
+app.delete('/users/:id', (req, res) => {
+    res.send('DELETE - Usado para remover dados');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,57 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, response } from 'express';
 
 const app: Express = express();
 const port = 3002;
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Meu primeiro server com Express e Typescript');
+app.get('/users', (req: Request, res: Response) => {
+  res.send('GET - Usado para obter dados');
+});
+
+app.get('/sum/:num1/:num2', (req: Request, res: Response) => {
+  const sum = Number(req.params.num1) + Number(req.params.num2);
+  res.send(`O resultado da soma é: ${sum}`);
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  
+  //FINGE QUE EU FUI NO BANCO E TROUXE ESSAS INFOS
+  const user = {
+        "user": {        
+            "nome": "Nilton",
+            "telefone": "249888888",
+            "endereco": "Rua sei que lá, 0, Tres Rios, RJ"
+        }
+      };
+  
+  const response = {
+    "status-code": 200,
+    "message": "Usuário encontrado",
+    "data": user
+  };
+
+  res.send(response);
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  //Parte do codigo responsavel por chamar um controller
+  //que chamar um model, que vai persistir a informação no banco.
+  var user = req.body.user;
+  user.id = 1;
+  const response = {
+    "status-code": 201,
+    "message": "Usuário criado com sucesso"
+    "user-registered": user
+  }
+  res.send(response);
+});
+
+app.put('/users/:id', (req: Request, res: Response) => {
+  res.send('PUT - Usado para atualizar dados');
+});
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+  res.send('DELETE - Usado para remover dados');
 });
 
 app.listen(port, () => {
